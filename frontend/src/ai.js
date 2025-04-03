dee56faf2ae177f053867802441a106c3e5957bf
Hevele Adam <heveleadam1@gmail.com>
2025-04-03 23:25:03 +0100

    API KEY UPDATED

--- frontend/src/ai.js
+++ frontend/src/ai.js
@@ -1,11 +1,14 @@
+const apiUrl = import.meta.env.VITE_API_URL
+
 export async function getRecipeFromChefClaude(ingredientsArr) {
-    const response = await fetch("http://localhost:3001/api/recipe", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ ingredients: ingredientsArr }),
-    })
-  
-    const data = await response.json()
-    return data.recipe
-  }
-  
+  const response = await fetch(`${apiUrl}/api/recipe`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ ingredients: ingredientsArr })
+  })
+
+  const data = await response.json()
+  return data.recipe
+}
