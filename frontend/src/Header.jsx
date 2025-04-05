f23e8ddc9c33bb4e947d113d1ada530deb1b49e6
Hevele Adam <heveleadam1@gmail.com>
2025-04-05 15:30:27 +0100

    Changing the logo

--- frontend/src/Header.jsx
+++ frontend/src/Header.jsx
@@ -1,9 +1,10 @@
 import chefClaudeIcon from './images/chef-claude-icon.png';
+import chefClaudeIcon1 from './images/ai-chef.png';
 
 export default function Header() {
   return (
     <header className="header">
-      <img className="logo" src={chefClaudeIcon} alt="icon" />
+      <img className="logo" src={chefClaudeIcon1} alt="icon" />
       <h1>Chef Claude</h1>
     </header>
   );
